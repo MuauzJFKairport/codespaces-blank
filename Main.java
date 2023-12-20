4db3e11e5919baa98eac00b1031ac03a897424a6
MuauzJFKairport <146112045+MuauzJFKairport@users.noreply.github.com>
2023-12-20 16:56:50 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Stuff stuff = new Stuff(12);
+        System.out.println(stuff.getValue());
+    }
+} 
---
+++ Stuff.java
@@ -0,0 +1,11 @@
+public class Stuff{
+    private int value;
+
+    public Stuff(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
